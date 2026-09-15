a8c6ffde9ce13068ebbc3c6c1388db0114270451
SeaSeaDries <257383486+SeaSeaDries@users.noreply.github.com>
2026-09-14 21:35:20 -0500

    Add Messenger.py and file_name.txt

---
+++ Messenger.py
@@ -0,0 +1,19 @@
+import os
+name = "Cc"
+
+file_name = input("Enter a messenger file name that ends in .txt: ")
+
+if os.path.exists(file_name):
+    file = open(file_name, "r")
+    print(file.read()) 
+    file.close()
+else:
+    print(f"\n{file_name} does not exist. A new file will be created instead")
+
+    new_message = input("Enter a message: ")
+
+    file = open(file_name, "a")
+    file.write(f"[{name}]: {new_message}\n")
+    file.close()
+
+    print("New message has been saved")
